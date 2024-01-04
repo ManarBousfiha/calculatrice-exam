23413f99c043f6b2039c2f790e9c5a155e3e507a
ETTIFOURI El Hassane <h.ettifouri@gmail.com>
2024-01-05 00:50:40 +0100

    Correction de la fonction d'addition et ajout des appels associés

--- src/main/java/ma/ehei/calculatrice/App.java
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -3,5 +3,9 @@ package ma.ehei.calculatrice;
 public class App {
 	public static void main(String[] args) {
 		System.out.println(Calculatrice.add(5, 5));
+		Calculatrice calculatrice = new Calculatrice();
+        int resultatAddition = calculatrice.addition(5, 7);
+        System.out.println("Résultat de l'addition : " + resultatAddition);
+    
 	}
 }
